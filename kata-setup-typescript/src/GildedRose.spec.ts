5f6d9d7697b260b957034310a8f2978e0f6e9be2
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2023-04-27 10:20:09 +0200

    Tests

--- kata-setup-typescript/src/GildedRose.spec.ts
+++ kata-setup-typescript/src/GildedRose.spec.ts
@@ -1,13 +1,14 @@
 import { describe, it, expect } from "vitest"
 import { GildedRose } from "./GildedRose.js"
 import { Item } from "./Item.js"
+import { log } from "util"
 
 const DEFAULT_ITEM_NAME = "item-name"
 const DEFAULT_ITEM_QUALITY = 5
 const DEFAULT_ITEM_SELLIN = 5
 
-const ITEM_MOTHER = (name?: Item["name"], quality?: Item["quality"], sellIn?: Item["quality"]) => {
-  return new Item(name ?? DEFAULT_ITEM_NAME, quality ?? DEFAULT_ITEM_QUALITY, sellIn ?? DEFAULT_ITEM_SELLIN)
+const createGildedRoseBuilder = ({ name, quality, sellIn }: { name?: string; quality?: number; sellIn?: number }) => {
+  return new Item(name ?? DEFAULT_ITEM_NAME, sellIn ?? DEFAULT_ITEM_SELLIN, quality ?? DEFAULT_ITEM_QUALITY)
 }
 
 describe("Gilded Rose", () => {
@@ -19,8 +20,28 @@ describe("Gilded Rose", () => {
 })
 describe("Basic items", () => {
   it("should decrease in 1 unit quality and sellIn", () => {
-    const basicItem = new GildedRose([new Item("basic-item", 1, 1)])
-    const updatedBasicItem = basicItem.updateQuality()
-    expect(updatedBasicItem[0].quality).toBe(0)
+    const gildedRose = new GildedRose([createGildedRoseBuilder({})])
+
+    const updatedGildedRose = gildedRose.updateQuality()
+
+    expect(updatedGildedRose[0].quality).toBe(DEFAULT_ITEM_QUALITY - 1)
+    expect(updatedGildedRose[0].sellIn).toBe(DEFAULT_ITEM_SELLIN - 1)
+  })
+
+  it("should decrease quality faster if sellIn equals to 0", () => {
+    const gildedRose = new GildedRose([createGildedRoseBuilder({ sellIn: 1 })])
+
+    gildedRose.updateQuality()
+    const updatedGildedRoseDay2 = gildedRose.updateQuality()
+
+    expect(updatedGildedRoseDay2[0].quality).toBe(DEFAULT_ITEM_QUALITY - 3)
+  })
+
+  it("quality shouldn't be negative", () => {
+    const gildedRose = new GildedRose([createGildedRoseBuilder({ quality: 0 })])
+
+    const updatedGildedRose = gildedRose.updateQuality()
+
+    expect(updatedGildedRose[0].quality).not.toBeLessThan(0)
   })
 })
