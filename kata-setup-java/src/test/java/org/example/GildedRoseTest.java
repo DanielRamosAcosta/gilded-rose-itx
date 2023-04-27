b8a3981af78ae5f4672edb025e9fd0c8fa27f70c
Gustavo Gutierrez <gustavogut1993@gmail.com>
2023-04-27 10:04:02 +0200

    First tests implemented

--- kata-setup-java/src/test/java/org/example/GildedRoseTest.java
+++ kata-setup-java/src/test/java/org/example/GildedRoseTest.java
@@ -6,10 +6,42 @@ import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
   @Test
-  void foo() {
-    Item[] items = new Item[] { new Item("fixme", 0, 0) };
+  void decreasesQualityByOne() {
+    Item[] items = new Item[] { new Item("some-random-item", 5, 5) };
     GildedRose app = new GildedRose(items);
+
     app.updateQuality();
-    assertEquals("fixme", app.items[0].name);
+
+    assertEquals(4, app.items[0].quality);
+  }
+
+  @Test
+  void decreasesSellInByOne() {
+    Item[] items = new Item[] { new Item("some-random-item", 5, 5) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(4, app.items[0].sellIn);
+  }
+
+  @Test
+  void decreasesQualityByTwoWhenExpired() {
+    Item[] items = new Item[] { new Item("some-random-item", 0, 5) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(3, app.items[0].quality);
+  }
+
+  @Test
+  void qualityDoesNotGetNegative() {
+    Item[] items = new Item[] { new Item("some-random-item", 0, 1) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(0, app.items[0].quality);
   }
 }
