2623e772ac79bb27e87cd624a0368e9906acc6dd
Natalia Iglesias Torres <nataliait@ext.zara.com>
2023-04-27 10:13:22 +0200

    Add updated aged quality

--- kata-setup-typescript/src/GildedRose.spec.ts
+++ kata-setup-typescript/src/GildedRose.spec.ts
@@ -2,12 +2,18 @@ import { describe, it, expect } from "vitest"
 import { GildedRose } from "./GildedRose.js"
 import { Item, ItemType } from "./Item.js"
 
-const dataSet =[new Item("Aged Brie", 0, 0, ItemType.Aged),new Item("Sulfuras, Hand of Ragnaros", 0, 0, ItemType.Legendary), new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0, ItemType.Backstage) ];
+const buildADataSet = () => [new Item("Aged Brie", 10, 0, ItemType.Aged),new Item("Sulfuras, Hand of Ragnaros", 0, 0, ItemType.Legendary), new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0, ItemType.Backstage) ];
 
 describe("Gilded Rose", () => {
   it("should foo", () => {
-    const gildedRose = new GildedRose(dataSet)
+    const gildedRose = new GildedRose(buildADataSet())
     const items = gildedRose.updateQuality()
     expect(items[0].name).toBe("Aged Brie")
+  })
+  it("should updated quality successfully", () => {
+    const gildedRose = new GildedRose(buildADataSet())
+    const items = gildedRose.updateQuality()
+    console.log("items", items[0].quality)
+    expect(items[0].quality).toBe(1)
   })
 })
