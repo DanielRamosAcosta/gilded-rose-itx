184033fdc575ae616ea0cfe9cd0f6067079b29ca
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-04-27 09:58:52 +0200

    Add item types

--- kata-setup-typescript/src/GildedRose.spec.ts
+++ kata-setup-typescript/src/GildedRose.spec.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from "vitest"
 import { GildedRose } from "./GildedRose.js"
-import { Item } from "./Item.js"
+import { Item, ItemType } from "./Item.js"
 
 describe("Gilded Rose", () => {
   it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("fixme", 0, 0)])
+    const gildedRose = new GildedRose([new Item("fixme", 0, 0, ItemType.Aged)])
     const items = gildedRose.updateQuality()
     expect(items[0].name).toBe("fixme")
   })
--- kata-setup-typescript/src/Item.ts
+++ kata-setup-typescript/src/Item.ts
@@ -1,11 +1,23 @@
+export enum ItemType {
+  Aged,
+  Legendary,
+  Backstage,
+}
+
 export class Item {
   name: string
   sellIn: number
   quality: number
+  type: ItemType
 
-  constructor(name: string, sellIn: number, quality: number) {
+  constructor(name: string, sellIn: number, quality: number, type: ItemType) {
     this.name = name
     this.sellIn = sellIn
     this.quality = quality
+    this.type = type
+  }
+
+  isType(type: ItemType): boolean {
+    return this.type === type
   }
 }
