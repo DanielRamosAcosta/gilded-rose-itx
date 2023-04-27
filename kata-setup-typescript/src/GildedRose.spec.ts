414d7591f6947ba8fcd0cef388836c7022f02b00
Natalia Iglesias Torres <nataliait@ext.zara.com>
2023-04-27 10:02:42 +0200

    Add dataSet test

--- kata-setup-typescript/src/GildedRose.spec.ts
+++ kata-setup-typescript/src/GildedRose.spec.ts
@@ -2,10 +2,12 @@ import { describe, it, expect } from "vitest"
 import { GildedRose } from "./GildedRose.js"
 import { Item, ItemType } from "./Item.js"
 
+const dataSet =[new Item("Aged Brie", 0, 0, ItemType.Aged),new Item("Sulfuras, Hand of Ragnaros", 0, 0, ItemType.Legendary), new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0, ItemType.Backstage) ];
+
 describe("Gilded Rose", () => {
   it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("fixme", 0, 0, ItemType.Aged)])
+    const gildedRose = new GildedRose(dataSet)
     const items = gildedRose.updateQuality()
-    expect(items[0].name).toBe("fixme")
+    expect(items[0].name).toBe("Aged Brie")
   })
 })
