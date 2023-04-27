b9135e74cffca9cf3f33892d9f0ced7c07f930e4
Emilio <emili.tfe@gmail.com>
2023-04-27 11:47:45 +0200

    feat: improve conditional

--- kata-setup-typescript/src/Item.BackstagePasses.ts
+++ kata-setup-typescript/src/Item.BackstagePasses.ts
@@ -13,11 +13,11 @@ export class BackstagePasses extends Item implements Itemer {
       this.quality = this.quality + 1
       return
     }
-    if (this.sellIn < 6) {
+    if (this.sellIn > 0 && this.sellIn < 6) {
       this.quality = this.quality + 3
       return
     }
-    if (this.sellIn < 11) {
+    if (this.sellIn > 0 && this.sellIn < 11) {
       this.quality = this.quality + 2
       return
     }
