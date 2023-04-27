452263189a49f69e3458757d056969d65fd5aabf
Alberto Barrio Louzao <albertoblou@ext.zara.com>
2023-04-27 10:15:40 +0200

    Adding test

--- kata-setup-java/src/test/java/org/example/GildedRoseTest.java
+++ kata-setup-java/src/test/java/org/example/GildedRoseTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
+
+  private final String AGED_BRIE = "Aged Brie";
+
+  private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
   @Test
   void decreasesQualityByOne() {
     Item[] items = new Item[] { new Item("some-random-item", 5, 5) };
@@ -43,5 +48,55 @@ class GildedRoseTest {
     app.updateQuality();
 
     assertEquals(0, app.items[0].quality);
+  }
+
+  @Test
+  void brieIncreasesQuality() {
+    Item[] items = new Item[] { new Item(AGED_BRIE, 10, 5) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(6, app.items[0].quality);
+  }
+
+  @Test
+  void brieIncreasesDoubleQualityAfterSellIn() {
+    Item[] items = new Item[] { new Item(AGED_BRIE, 0, 5) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(7, app.items[0].quality);
+  }
+
+  @Test
+  void qualityDoesNotGreaterThan50() {
+    Item[] items = new Item[] { new Item(AGED_BRIE, 0, 50) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(50, app.items[0].quality);
+  }
+
+  @Test
+  void sulfurasDoesNotChangeQuality() {
+    Item[] items = new Item[] { new Item(SULFURAS, 0, 10) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(10, app.items[0].quality);
+  }
+
+  @Test
+  void sulfurasDoesNotChangeSellIn() {
+    Item[] items = new Item[] { new Item(SULFURAS, 2, 10) };
+    GildedRose app = new GildedRose(items);
+
+    app.updateQuality();
+
+    assertEquals(2, app.items[0].sellIn);
   }
 }
