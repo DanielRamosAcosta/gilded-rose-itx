cbdbde5e853b9872b80f955d20f84cad602ee4f7
Daniel Salinas Merino <danielsalinas305@gmail.com>
2023-04-27 10:15:32 +0200

    feat: Another test

--- kata-setup-typescript/src/GildedRose.spec.ts
+++ kata-setup-typescript/src/GildedRose.spec.ts
@@ -3,10 +3,19 @@ import { GildedRose } from "./GildedRose.js"
 import { Item } from "./Item.js"
 
 describe("Gilded Rose", () => {
-  it("a unknown item must decrement sellIn and quality by one", () => {
+  it("an unknown item must decrement sellIn and quality by one each day", () => {
     const gildedRose = new GildedRose([new Item("apple", 5, 5)])
     const items = gildedRose.updateQuality()
     expect(items[0].sellIn).toBe(4)
     expect(items[0].quality).toBe(4)
+  })
+
+  it("an unknown item must decrement quality by two if sellIn is expired (0 or less)", () => {
+    const gildedRose = new GildedRose([new Item("fresh apple", 0, 10)])
+
+    const items = gildedRose.updateQuality()
+
+    expect(items[0].sellIn).toBe(-1)
+    expect(items[0].quality).toBe(8)
   })
 })
